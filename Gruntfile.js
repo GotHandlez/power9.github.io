bff8bae5f4f1e81404e42d45c9c7f48d6fdd1728
Daniel Pao <issac.power@gmail.com>
2014-06-10 21:55:43 -0700

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,7 @@ module.exports = function (grunt) {
       pages: {
         options: {
           remote:'https://github.com/power9/power9.github.io.git',
-          branch: 'gh-pages'
+          branch: 'try2'
         }
       }
     },
